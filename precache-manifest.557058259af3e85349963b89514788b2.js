8d7d876a4cfb6809c952875837a58896f1f713dc
bravotango <seattleriot@hotmail.com>
2022-02-19 13:13:14 -0800

    Updates

---
+++ precache-manifest.557058259af3e85349963b89514788b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc8723b4746615a6586dea0b7e27940",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "a08bc0e640094f26e49c",
+    "url": "/portfolio-react/static/css/main.04b4a0fd.chunk.css"
+  },
+  {
+    "revision": "391d0676d98f7aa5c9ec",
+    "url": "/portfolio-react/static/js/2.716ea5bf.chunk.js"
+  },
+  {
+    "revision": "63fbe989101a4b7a3114",
+    "url": "/portfolio-react/static/js/3.13844b13.chunk.js"
+  },
+  {
+    "revision": "a08bc0e640094f26e49c",
+    "url": "/portfolio-react/static/js/main.ddb613b9.chunk.js"
+  },
+  {
+    "revision": "afed102a61b4b97562ef",
+    "url": "/portfolio-react/static/js/runtime~main.ca44fba5.js"
+  },
+  {
+    "revision": "8513698c884b56cc9c20d56b307e2180",
+    "url": "/portfolio-react/static/media/bg-ana.8513698c.jpg"
+  },
+  {
+    "revision": "1e6213405ed38532461169550dad4d42",
+    "url": "/portfolio-react/static/media/bg.1e621340.jpg"
+  },
+  {
+    "revision": "541386de2c45c5d1b80c13536dc5e02a",
+    "url": "/portfolio-react/static/media/face-hood.541386de.png"
+  },
+  {
+    "revision": "5dafbba425a6bd77547759e522a3cae9",
+    "url": "/portfolio-react/static/media/joker.5dafbba4.png"
+  },
+  {
+    "revision": "6da163f1b78ea07f0ca31a1d378fb442",
+    "url": "/portfolio-react/static/media/movie.6da163f1.jpg"
+  },
+  {
+    "revision": "8a3e15d5ae5d179c25f9d6f36a8964a4",
+    "url": "/portfolio-react/static/media/office-characters.8a3e15d5.png"
+  },
+  {
+    "revision": "f56ac32808e6eaddbb8111e6ad1d2b5e",
+    "url": "/portfolio-react/static/media/tech-blog.f56ac328.png"
+  },
+  {
+    "revision": "de66b19988bbb5ab2da462ae28d4dfbf",
+    "url": "/portfolio-react/static/media/walk.de66b199.jpg"
+  },
+  {
+    "revision": "7ce091aa57d26261f7417c59a89acebc",
+    "url": "/portfolio-react/static/media/weather.7ce091aa.jpg"
+  },
+  {
+    "revision": "97433c4cf047328d6d3e8675e231a82c",
+    "url": "/portfolio-react/static/media/workday.97433c4c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ee2ce425b85b680f8897cc1e7fcbd27.js"
+  "/portfolio-react/precache-manifest.557058259af3e85349963b89514788b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
