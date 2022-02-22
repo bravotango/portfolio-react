4c1029ee35ad1fcdf45b711d34b5c1352fbf9213
bravotango <seattleriot@hotmail.com>
2022-02-22 00:40:45 -0800

    Updated resume

--- src/components/main/aboutMe/AboutMe.js
+++ src/components/main/aboutMe/AboutMe.js
@@ -19,7 +19,7 @@ const AboutMe = () => {
           <div>
             <p>
               I love collaborating, brainstorming, dreaming. I love transforming
-              ideas into applications for anyone on the web.
+              ideas into applications available to everyone on the web.
             </p>
             <p>My focus when creating solutions is simplicity.</p>
           </div>
