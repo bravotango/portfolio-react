1ace08b501b8469c8ca9c77ad997c0014d69ec37
bravotango <seattleriot@hotmail.com>
2023-01-31 16:54:52 -0800

    Added work history

--- src/components/main/aboutMe/AboutMe.js
+++ src/components/main/aboutMe/AboutMe.js
@@ -27,6 +27,40 @@ const AboutMe = () => {
         <div>
           <Skills />
         </div>
+        <div>
+          <h3>Work History</h3>
+          <h4>Microsoft | Actalent Services</h4>
+          <p>
+            Working as a Software Development Engineer at Microsoft has been
+            wonderful. Experiencing the campus before COVID is something I will
+            always cherish (on my first day, I remember eating ice cream with my
+            new team in the most amazing tree house). I was surrounded with
+            great people. My team grew a US-based pilot project to become
+            available for commercial customers world-wide. When I started, we
+            worked in Angular. As the Angular codebase retired, we built our new
+            experience in React available on Microsoft Admin Center. Both code
+            bases use TypeScript and deploy code with Azure & Git. I wrote unit
+            tests with Jest. I created Figma prototypes for the team when we did
+            not have a dedicated UX designer. As the project went international,
+            I learned more about localization, accessibility, and the fine
+            details of world-wide address validation.
+          </p>
+          <h4>Premera Blue Cross</h4>
+          <p>
+            Working as a Software Developer at Premera was quite a ride. Over 16
+            years solidified this as my longest tenure. I raised my family
+            during my time at Premera and many of my peers felt like family. I
+            started building interfaces for Premera in 2002 before CSS
+            frameworks were a thing. Premera had many provider relationships
+            whose systems required us to support IE 8 (even past the date
+            Microsoft supported it). I worked closely with Marketing & Branding
+            as I was the only UI developer for many years. As we grew, a UI team
+            was created. As part of the UI team, we adopted the Bootstrap CSS
+            framework & created reusable Angular components for teams to consume
+            (carousels, alerts, cards, accordions, etc.). I became the UI lead
+            for the Provider team before my departure.
+          </p>
+        </div>
       </div>
     </section>
   );
